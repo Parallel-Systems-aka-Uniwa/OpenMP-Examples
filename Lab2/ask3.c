e5464886f9ce4cdef963d54663fcc0f4048ada91
Tiger <billath131908@gmail.com>
2024-11-05 15:07:46 +0200

    OpenMP

---
+++ Lab2/ask3.c
@@ -0,0 +1,29 @@
+#include <omp.h>
+#include <stdio.h>
+#define N 10000
+
+main ()
+{
+            int tid, i, sum=0;
+            omp_set_num_threads(8);
+
+            #pragma omp parallel private(i, tid)
+            {
+                        tid = omp_get_thread_num();
+                        for (i=0; i < N; i++)
+                        {
+                                    #pragma omp atomic
+                                    sum=sum+i;
+                        }
+                        #pragma omp barrier
+                        printf("Thread %d reports: Sum is %d\n", tid, sum);
+                        #pragma omp barrier
+                        for (i=0; i < N; i++)
+                        {
+                                    #pragma omp atomic
+                                    sum=sum-i;
+                        }
+                        #pragma omp barrier
+                        printf("Thread %d reports: Sum is %d - goodbye!\n", tid, sum);
+            }
+}
